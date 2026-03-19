c408ca9110bb84deb52c61295944bf8f5c6c151a
Tahir Asadli <tahir-asadov@outlook.com>
2026-03-19 20:47:07 +0400

    Update home page

---
+++ main.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function CustomClassExtension(Splide, Components, options) {
+    function applyClasses(newIndex) {
+      const slides = Components.Elements.slides;
+
+      slides.forEach((slide, i) => {
+        slide.classList.remove('is-active', 'is-next', 'is-prev');
+
+        if (i === newIndex) slide.classList.add('is-active');
+        if (i === newIndex + 1) slide.classList.add('is-next');
+        if (i === newIndex - 1) slide.classList.add('is-prev');
+      });
+    }
+
+    return {
+      mount() {
+        // Apply on initial load
+        applyClasses(Splide.index);
+
+        // Apply BEFORE each move
+        Splide.on('move', (newIndex) => {
+          applyClasses(newIndex);
+        });
+      }
+    };
+  }
+
+
+  if (document.querySelector('.testimonials-slider')) {
+
+    testimonialSplide = new Splide('.testimonials-slider', {
+      direction: 'ttb',
+      autoHeight: true,
+      height: '400px',
+      padding: '2rem',
+      focus: 'center',
+      wheel: true,
+      trimSpace: false,
+      type: 'loop',
+      perPage: 1,
+      perMove: 1,
+      pagination: true,
+      arrows: true,
+      autoplay: true,
+      interval: 30000,
+    });
+
+    const count = document.querySelectorAll('.testimonials-slider .splide__list').length ?? 0
+    // testimonials-pagination
+
+    // testimonialSplide.on('move', function (newIndex, oldIndex, destIndex) {
+    //   const slides = testimonialSplide.Components.Elements.slides;
+
+    //   // Remove existing classes
+    //   slides.forEach(slide => {
+    //     slide.classList.remove('is-active', 'is-next', 'is-prev');
+    //   });
+
+    //   // Add classes before animation
+    //   if (slides[newIndex]) {
+    //     slides[newIndex].classList.add('is-active');
+    //   }
+    //   if (slides[newIndex + 1]) {
+    //     slides[newIndex + 1].classList.add('is-next');
+    //   }
+    //   if (slides[newIndex - 1]) {
+    //     slides[newIndex - 1].classList.add('is-prev');
+    //   }
+    // });
+    testimonialSplide.mount({ CustomClassExtension });
+  }
+});
